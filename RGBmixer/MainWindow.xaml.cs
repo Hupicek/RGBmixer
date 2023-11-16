9b4d01f774510258ec8da2a98f6613d7bb5fad22
jan.ptacek <jan.ptacek@VT1-15>
2023-11-16 13:19:49 +0100

    Přidejte soubory projektu.

---
+++ RGBmixer/MainWindow.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media;
+
+namespace RGBmixer
+{
+    public partial class MainWindow : Window
+    {
+
+
+        private void sliderR_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
+        {
+            int hodnotaR = (int)sliderR.Value;
+            int hodnotaG = (int)sliderG.Value;
+            int hodnotaB = (int)sliderB.Value;
+
+            BarvaL.Background = new SolidColorBrush(Color.FromRgb((byte)hodnotaR, (byte)hodnotaG, (byte)hodnotaB));
+        }
+        private void sliderG_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
+        {
+            int hodnotaR = (int)sliderR.Value;
+            int hodnotaG = (int)sliderG.Value;
+            int hodnotaB = (int)sliderB.Value;
+
+            BarvaL.Background = new SolidColorBrush(Color.FromRgb((byte)hodnotaR, (byte)hodnotaG, (byte)hodnotaB));
+        }
+        private void sliderB_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
+        {
+            int hodnotaR = (int)sliderR.Value;
+            int hodnotaG = (int)sliderG.Value;
+            int hodnotaB = (int)sliderB.Value;
+
+            BarvaL.Background = new SolidColorBrush(Color.FromRgb((byte)hodnotaR, (byte)hodnotaG, (byte)hodnotaB));
+        }
+
+        private void sliderR_ValueChanged(object sender, RoutedPropertyChangedEventArgs<T> e)
+        {
+
+        }
+    }
+}
